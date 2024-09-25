06a3701d42ce0e8a0b9cdc43a69dfb2a5e45d3cd
namidoidez <namidoidez@gmail.com>
2024-09-25 19:04:04 +0300

    feat(TaskAreaPresenter): adding encapsulation; decomposition of the init function

--- src/presenter/task-area-presenter.js
+++ src/presenter/task-area-presenter.js
@@ -1,30 +1,47 @@
 import TaskListComponent from '../view/task-list-component.js';
 import TaskComponent from '../view/task-component.js';
-import TaskAreaComponent from '../view/task-area-component.js';
 import ButtonClearComponent from '../view/button-clear-component.js';
 import {render} from '../framework/render.js';
 
 
-export default class TasksAreaPresenter {   
+export default class TaskAreaPresenter {
+    #taskModel = null;
+    #taskArea = null;
+    #taskAreaContainer = null;
+
     constructor(taskModel, taskAreaContainer) {
-        this.taskModel = taskModel;
-        this.taskAreaContainer = taskAreaContainer;
+        this.#taskModel = taskModel;
+        this.#taskAreaContainer = taskAreaContainer;
     }
 
-    init() {       
+    init() {
+        this.#taskArea = this.#taskModel.getTasks();
+        this.#renderTaskArea();
+    }
+    
+    #renderTaskArea() {
         var taskListContainer = null;
-        this.taskModel.getTasks().forEach((taskList, index) => {
+
+        this.#taskArea.forEach((taskList, index) => {
             var taskColor = taskList.color;
             var taskListComponent = new TaskListComponent(taskList.title, taskColor);
-            render(taskListComponent, this.taskAreaContainer);
-
-            taskListContainer = this.taskAreaContainer.querySelectorAll(".tasks__list")[index];
-            taskList.items.forEach(item => {
-                var taskComponent = new TaskComponent(item.task, taskColor);
-                render(taskComponent, taskListContainer);
-            });        
+            render(taskListComponent, this.#taskAreaContainer);
+            
+            taskListContainer = this.#taskAreaContainer.querySelectorAll('.tasks__list')[index];
+            this.#renderTaskList(taskList.items, taskColor, taskListContainer);
         });
-        
+            
         render(new ButtonClearComponent(), taskListContainer);
+    }
+
+    #renderTaskList(taskList, taskColor, taskListContainer) {
+        taskList.forEach(item => {
+            this.#renderTask(item.task, taskColor, taskListContainer)
+        }); 
+    }
+
+    #renderTask(task, taskColor, container) {
+        var taskComponent = new TaskComponent(task, taskColor);
+        render(taskComponent, container);
     }
 }
