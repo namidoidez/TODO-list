14cb8c876a6729e76c37f09dcf40a8fd9c366b2f
namidoidez <namidoidez@gmail.com>
2024-10-15 22:39:45 +0300

    feat: adding details to the model, and adding the necessary constants to work on it

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,16 +1,30 @@
-export const taskArea = [
+import {generateId} from '../utils.js'
+
+
+const TaskListStatus = {
+    BACKLOG:   0,
+    INPROCESS: 1,
+    DONE:      2,
+    BASKET:    3,
+};
+
+var taskArea = [
     /* taskList */
     {
+        id: TaskListStatus.BACKLOG,
         title: 'Бэклог',
         color: 'black',
         items: [
             {
+                id: generateId(), 
                 task: 'Выучить JS',
             },  
             {
+                id: generateId(), 
                 task: 'Выучить React',
             },
             {
+                id: generateId(), 
                 task: 'Сделать домашку',
             },
         ]
@@ -18,13 +32,16 @@ export const taskArea = [
     
     /* taskList */
     {
+        id: TaskListStatus.INPROCESS,
         title: 'В процессе',
         color: 'royalblue',
         items: [ 
             {
+                id: generateId(), 
                 task: 'Выпить смузи',
             },
             {
+                id: generateId(), 
                 task: 'Попить воды',
             },
         ]
@@ -32,13 +49,16 @@ export const taskArea = [
     
     /* taskList */
     {
+        id: TaskListStatus.DONE,
         title: 'Готово',
         color: 'green',
         items: [
             {
+                id: generateId(), 
                 task: 'Позвонить маме',
             },  
             {
+                id: generateId(), 
                 task: 'Погладить кота',
             },
         ]
@@ -46,15 +66,20 @@ export const taskArea = [
     
     /* taskList */
     {
+        id: TaskListStatus.BASKET,
         title: 'Корзина',
         color: 'brown',
         items: [
             {
+                id: generateId(), 
                 task: 'Сходить погулять',
             },  
             {
+                id: generateId(), 
                 task: 'Прочитать Войну и Мир',
             },
         ]
     },
-]
+];
+
+export {TaskListStatus, taskArea};
