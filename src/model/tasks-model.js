0c10503427ba85533a100e8a2b56bdda7b729ee1
namidoidez <namidoidez@gmail.com>
2024-10-15 22:43:47 +0300

    feat(TasksModel): adding methods to interact with the model, and implementing the observer pattern

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,9 +1,39 @@
-import {taskArea} from '../mock/task.js'
+import {TaskListStatus, taskArea} from '../mock/task.js'
+import {generateId} from '../utils.js'
 
 export default class TasksModel {
     #tasks = taskArea;
+    #observers = [];
 
     get tasks() {
         return this.#tasks;
+    }
+
+    addTask(task) {
+        var newTask = {
+            id: generateId(), 
+            task: task,
+        }
+        this.#tasks[TaskListStatus.BACKLOG].items.push(newTask);
+        
+        this._notifyObservers();
+    }
+
+    removeTasksByStatus(taskListStatus) {
+        this.#tasks[taskListStatus].items = [];
+        
+        this._notifyObservers();
+    }
+
+    addObserver(observer) {
+        this.#observers.push(observer);
+    }
+
+    removeObserver(observer) {
+        this.#observers = this.#observers.filter((obs) => obs != observer);
+    }
+
+    _notifyObservers() {
+        this.#observers.forEach((obs) => obs());
     }
 }
