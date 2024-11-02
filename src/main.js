7aa0df8751bda139159b4255e35cab173cdf72c1
namidoidez <namidoidez@gmail.com>
2024-11-02 10:14:14 +0300

    feat: changing the application's entry point

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import HeaderComponent from './view/header-component.js';
 import FormAddTaskComponent from './view/form-add-task-component.js';
+import TasksApiService from './tasks-api-service.js';
+import TasksModel from './model/tasks-model.js';
 import TaskAreaPresenter from './presenter/task-area-presenter.js';
-import TasksModel from './model/tasks-model.js'
 import {render, RenderPosition} from './framework/render.js';
 
+
+const END_POINT = 'https://6720c87f98bbb4d93ca5fd76.mockapi.io';
 
 const bodyContainer= document.getElementsByTagName('body')[0];
 render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
@@ -11,7 +14,7 @@ render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
 const mainContainer = bodyContainer.getElementsByTagName('main')[0];
 render(new FormAddTaskComponent({onClick: handleNewTaskButtonClick}), mainContainer, RenderPosition.AFTERBEGIN);
 
-const tasksModel = new TasksModel();
+const tasksModel = new TasksModel({tasksApiService: new TasksApiService(END_POINT)});
 const taskAreaPresenter = new TaskAreaPresenter(tasksModel, mainContainer);
 taskAreaPresenter.init();
 
