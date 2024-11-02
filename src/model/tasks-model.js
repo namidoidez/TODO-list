8f5225f6a365231aed3b86abeee6a47d862a91ac
namidoidez <namidoidez@gmail.com>
2024-11-02 10:09:38 +0300

    feat(TasksModel): adding inheritance from Observable and aggregation of TasksApiService

--- src/model/tasks-model.js
+++ src/model/tasks-model.js
@@ -1,74 +1,137 @@
-import {taskArea} from '../mock/task.js'
-import {generateId} from '../utils.js'
-import {TaskListStatus} from '../const.js'
+import Observable from '../framework/observable.js';
+import IdGenerator from '../utils.js';
+import {TaskListStatus, UserAction, UpdateType} from '../const.js';
 
 
-export default class TasksModel {
-    #tasks = taskArea;
-    #observers = [];
+export default class TasksModel extends Observable {
+    #tasks = null;
+    #idGenerator = null;
+    #tasksApiService = null;
+
+    constructor({tasksApiService}) {
+        super();
+        this.#tasksApiService = tasksApiService;
+    }
+
+    async init() {
+        try {
+            var tasks = await this.#tasksApiService.tasks;
+            this.#tasks = tasks;
+            
+            var lastId = tasks.reduce((min, task) => {
+                return task.id < min ? task.id : min;
+            }, Infinity);
+            this.#idGenerator = new IdGenerator(lastId);
+        } 
+        catch(err) {
+            this.#tasks = [];
+        }
+
+        this._notify(UpdateType.INIT);
+    }
 
     get tasks() {
         return this.#tasks;
     } 
 
-    addTask(taskTitle) {
+    async addTask(taskTitle) {
         var newTask = {
-            id: generateId(), 
+            id: this.#idGenerator.generate(),
             title: taskTitle,
             status: TaskListStatus.BACKLOG
         };
-        this.#tasks.push(newTask);
-        
-        this._notifyObservers();
+
+        try {
+            var createdTask = await this.#tasksApiService.addTask(newTask);
+            this.#tasks.push(createdTask);
+            this._notify(UserAction.ADD_TASK, createdTask);
+            return createdTask;
+        } 
+        catch (err) {
+            console.error(`Ошибка при добавлении задачи на сервер: ${err}`);
+            throw err;
+        }
     }
 
-    moveTaskToEnd(taskId, targetStatus) {
+    async moveTaskToEnd(taskId, targetStatus) {
         var taskIndex = this.#tasks.findIndex((task) => task.id == taskId);
-        var task = this.#tasks.splice(taskIndex, 1)[0];
-        task.status = targetStatus;
-        this.#tasks.push(task);
-
-        this._notifyObservers();
+        
+        if (taskIndex != -1) {
+            var task = this.#tasks[taskIndex];
+            var oldStatus = task.status;
+            task.status = targetStatus;
+            
+            try {
+                var updatedTask = await this.#tasksApiService.updateTask(task);
+                Object.assign(task, updatedTask);
+                this.#tasks.splice(taskIndex, 1);
+                this.#tasks.push(task);
+                
+                this._notify(UserAction.UPDATE_TASK, {newStatus: targetStatus, oldStatus: oldStatus});
+            }
+            catch (err) {
+                task.status = oldStatus;
+                
+                console.error(`Ошибка при обновлении статуса задачи на сервера: ${err}`);
+                throw err;
+            }
+        }
     }
     
-    moveTask(taskId, targetTaskId, targetStatus) { 
+    async moveTask(taskId, targetTaskId, targetStatus) { 
         var currentTaskIndex = this.#tasks.findIndex((task) => task.id == taskId);
         var targetTaskIndex = this.#tasks.findIndex((task) => task.id == targetTaskId);
         
-        var currentTask = this.#tasks.splice(currentTaskIndex, 1)[0];
-        currentTask.status = targetStatus;
-        this.#tasks.splice(targetTaskIndex, 0, currentTask);
-        
-        this._notifyObservers();
+        if (currentTaskIndex != -1 && targetTaskIndex != -1) {
+            var currentTask = this.#tasks[currentTaskIndex];
+            var oldStatus = currentTask.status;
+            currentTask.status = targetStatus;
+            
+            try {
+                var updatedTask = await this.#tasksApiService.updateTask(currentTask);
+                Object.assign(currentTask, updatedTask);
+                this.#tasks.splice(currentTaskIndex, 1);
+                this.#tasks.splice(targetTaskIndex, 0, currentTask);
+                
+                this._notify(UserAction.UPDATE_TASK, {newStatus: targetStatus, oldStatus: oldStatus});
+            }
+            catch (err) {
+                currentTask.status = oldStatus;
+                
+                console.error(`Ошибка при обновлении статуса задачи на сервера: ${err}`);
+                throw err;
+            }
+        }
+    }
+
+    hasTasksByStatus(taskListStatus) {
+        return this.#tasks.some((task) => task.status == taskListStatus);
     }
 
     getTasksByStatus(taskListStatus) {
         return this.#tasks.filter((task) => task.status == taskListStatus);
     }
 
-    removeTasksByStatus(taskListStatus) {
-        var tasks = this.getTasksByStatus(taskListStatus);
-        tasks.forEach((task) => {
-            this.removeTask(task);
-        })
+    async removeTasksByStatus(taskListStatus) {
+        var basketTasks = this.getTasksByStatus(taskListStatus);
         
-        this._notifyObservers();
+        try {
+            await Promise.all(basketTasks.map((task) => this.#tasksApiService.removeTask(task.id)));
+            basketTasks.forEach((task) => {
+                this.#removeTask(task);
+            })
+            
+            this._notify(UserAction.REMOVE_TASK, UpdateType.MINOR);
+        }
+        catch (err) {
+            console.error(`Ошибка при удалении задачи на сервера: ${err}`);
+            throw err;
+        }
+        
     }
 
-    removeTask(task) {
+    #removeTask(task) {
         var taskIndex = this.#tasks.indexOf(task);
         this.#tasks.splice(taskIndex, 1);
-    }
-    
-    addObserver(observer) {
-        this.#observers.push(observer);
-    }
-
-    removeObserver(observer) {
-        this.#observers = this.#observers.filter((obs) => obs != observer);
-    }
-
-    _notifyObservers() {
-        this.#observers.forEach((obs) => obs());
     }
 }
