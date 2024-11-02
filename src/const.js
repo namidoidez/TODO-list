c1295db2bd6ee7fdcbd840fe0f3bef4f8a4c036c
namidoidez <namidoidez@gmail.com>
2024-11-02 10:08:49 +0300

    feat: creating constants for standardization and more convenient interaction between MVP components

--- src/const.js
+++ src/const.js
@@ -1,22 +1,33 @@
-const TaskListStatus = {
+export const TaskListStatus = {
     BACKLOG:   0,
     INPROCESS: 1,
     DONE:      2,
     BASKET:    3
 };
 
-const TaskListTitle = {
-    [TaskListStatus.BACKLOG]:   `Бэклог`,
-    [TaskListStatus.INPROCESS]: `В процессе`,
-    [TaskListStatus.DONE]:      `Готово`,
-    [TaskListStatus.BASKET]:    `Корзина`,
+export const TaskListTitle = {
+    [TaskListStatus.BACKLOG]:   'Бэклог',
+    [TaskListStatus.INPROCESS]: 'В процессе',
+    [TaskListStatus.DONE]:      'Готово',
+    [TaskListStatus.BASKET]:    'Корзина',
 };
 
-const TaskListColor = {
-    [TaskListStatus.BACKLOG]:   `black`,
-    [TaskListStatus.INPROCESS]: `royalblue`,
-    [TaskListStatus.DONE]:      `green`,
-    [TaskListStatus.BASKET]:    `brown`,
+export const TaskListColor = {
+    [TaskListStatus.BACKLOG]:   'black',
+    [TaskListStatus.INPROCESS]: 'royalblue',
+    [TaskListStatus.DONE]:      'green',
+    [TaskListStatus.BASKET]:    'brown',
 };
 
-export {TaskListStatus, TaskListTitle, TaskListColor};
+export const UserAction = {
+    UPDATE_TASK: 'UPDATE_TASK',
+    ADD_TASK:    'ADD_TASK',
+    REMOVE_TASK: 'REMOVE_TASK'
+};
+
+export const UpdateType = {
+    PATCH: 'PATCH',
+    MINOR: 'MINOR',
+    MAJOR: 'MAJOR',
+    INIT:  'INIT'
+};
